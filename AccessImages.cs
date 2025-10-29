a999db33a00a355d96d600a7cfa981cc4539d165
Wojciech Stachowiak <>
2025-10-29 14:34:52 +0100

    add AccessImage endpoint

---
+++ AccessImages.cs
@@ -0,0 +1,29 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.Functions.Worker.Http;
+
+namespace TripleAssignment;
+
+public class AccessImages
+{
+    private readonly ILogger<AccessImages> _logger;
+
+    public AccessImages(ILogger<AccessImages> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("AccessImages")]
+    public async Task<HttpResponseData> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req,
+            [BlobInput("images/image_{query.city}.jpg", Connection = "AzureWebJobsStorage")] Stream blobItem
+            )
+    {
+        _logger.LogInformation("City requested");
+
+        HttpResponseData response = req.CreateResponse(System.Net.HttpStatusCode.OK);
+        await blobItem.CopyToAsync(response.Body);
+
+        return response;
+    }
+}
